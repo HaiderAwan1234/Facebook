f8363f37a9545fdb87cd1051acefaa84a9970977
W10 <haiderharoon1234512345@gmail.com>
2025-03-18 22:27:51 +0500

    signup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html", // Add your HTML or template files here
+    "./src/**/*.{js,jsx,ts,tsx}", // Add source files if needed
+  ],
+  theme: {
+    extend: {}, // You can add custom configurations here (optional)
+  },
+  plugins: [], // Add plugins here if needed
+};

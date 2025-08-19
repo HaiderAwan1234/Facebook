ff23a67e119803d4cae06600f4af9bfe10f8431d
W10 <haiderharoon1234512345@gmail.com>
2025-08-19 15:29:56 +0500

    Like,Dislike

--- Backend/controller/post_controller.js
+++ Backend/controller/post_controller.js
@@ -22,17 +22,28 @@ export const getPost = async (req, res) => {
 
 export const postEmogi = async (req, res) => {
   const { emogi } = req.body;
-  const { post_id, user_id } = req.params;
+  const { user_id, post_id } = req.params;
 
   const findPost = await Post.findById(post_id);
 
-  if (!post_id) {
+  if (!findPost) {
     res.status(404);
     throw new Error("Post not found !!");
   }
 
-  findPost.reaction.push({ type: emogi, id: user_id });
-  await findPost.save();
+  // Find the index of the existing reaction
+  const checkLike = findPost.reaction.findIndex((item) => {
+    return item.id == user_id && item.type === emogi;
+  });
 
+  if (checkLike === -1) {
+    // If not found, add the reaction
+    findPost.reaction.push({ type: emogi, id: user_id });
+  } else {
+    // If found, remove it using splice
+    findPost.reaction.splice(checkLike, 1);
+  }
+
+  await findPost.save();
   res.send(findPost);
 };
